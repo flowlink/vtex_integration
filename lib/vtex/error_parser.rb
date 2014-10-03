ff9c1b0a22de18ac432a52033220e8055d8a52ed
Reinaldo Braga <reinaldob@gmail.com>
2014-10-03 16:20:43 -0300

    IN-607 Fix error on error_parser for vtex

--- lib/vtex/error_parser.rb
+++ lib/vtex/error_parser.rb
@@ -1,7 +1,7 @@
 module VTEX
   class ErrorParser
     def self.response_has_errors?(response)
-      response.code == 400 || response.code == 401 || !response['status'].nil?
+      response.code == 400 || response.code == 401
     end
   end
 end
