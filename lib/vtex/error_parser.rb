5b85ea60b4e40299e1870b9f8e86ce2e00e16b17
Reinaldo Braga <reinaldob@gmail.com>
2014-10-09 18:42:25 -0300

    IN-607 Changes error parser to support SOAP too

--- lib/vtex/error_parser.rb
+++ lib/vtex/error_parser.rb
@@ -1,7 +1,12 @@
 module VTEX
   class ErrorParser
     def self.response_has_errors?(response)
-      response.code == 400 || response.code == 401 || response.code == 404 || response.code == 500 ||(response.respond_to?(:has_key?) && response.has_key?('error'))
+      response.http_error? ||
+      (response.respond_to?(:code) && (response.code == 400 ||
+                                       response.code == 401 ||
+                                       response.code == 404 ||
+                                       response.code == 500)) ||
+      (response.respond_to?(:has_key?) && response.has_key?('error'))
     end
   end
 end
