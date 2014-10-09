b501513c5ab3e782846d288daaf7dfe02836c4b3
Reinaldo Braga <reinaldob@gmail.com>
2014-10-09 15:25:57 -0300

    IN-647 Adds more codes to error parser

--- lib/vtex/error_parser.rb
+++ lib/vtex/error_parser.rb
@@ -1,7 +1,7 @@
 module VTEX
   class ErrorParser
     def self.response_has_errors?(response)
-      response.code == 400 || response.code == 401
+      response.code == 400 || response.code == 401 || response.code == 404 || response.code == 500 ||(response.respond_to?(:has_key?) && response.has_key?('error'))
     end
   end
 end
