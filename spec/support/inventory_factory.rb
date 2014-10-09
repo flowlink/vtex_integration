cdb6989b47b682fab3847ef80d82aee4605e2d5f
Reinaldo Braga <reinaldob@gmail.com>
2014-10-09 14:33:18 -0300

    IN-607 Adds factories for orders, inventory and parameters

---
+++ spec/support/inventory_factory.rb
@@ -0,0 +1,10 @@
+module Factories
+  def self.inventory
+    {
+      'id'         =>  '12876920',
+      'location'   =>  '1_1',
+      'product_id' =>  '2000005',
+      'quantity'   =>  93
+    }
+  end
+end
--- spec/support/parameters_factory.rb
+++ spec/support/parameters_factory.rb
@@ -2,8 +2,10 @@ module Factories
   def self.parameters
     {
       'vtex_site_id'   => 'spree',
-      'vtex_app_key'   => 'spree@spreecommerce.com',
-      'vtex_app_token' => '123',
+      'vtex_app_key'   => 'spree-key',
+      'vtex_app_token' => 'token-123',
+      'vtex_password'  => 'spree-pwd',
+      'vtex_poll_order_timestamp' => '2014-10-05T14:49:00-03:00'
     }
   end
 end
