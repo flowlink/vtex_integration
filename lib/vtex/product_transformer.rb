52fff29d048ac83479c824e315090bd498de1e6f
Washington Luiz <huoxito@gmail.com>
2014-12-09 17:35:35 -0300

    Dont put master sku under product.variants

--- lib/vtex/product_transformer.rb
+++ lib/vtex/product_transformer.rb
@@ -4,6 +4,11 @@ module VTEX
       def map(products, client)
         products.map do |product|
           product_id = product.delete(:id)
+          stock_units = client.get_skus_by_product_id product_id
+
+          parent_sku = find_parent_sku stock_units, product_id, client
+          product = product.merge parent_sku
+
           {
             id: product_id,
             channel: 'vtex',
@@ -15,16 +20,29 @@ module VTEX
             meta_description: product.delete(:title),
             is_visible: product.delete(:is_visible),
             is_active: product.delete(:is_active),
-            variants: map_variants(client, product_id)
+            variants: map_variants(stock_units, product_id, client)
           }.merge product
         end
       end
 
-      def map_variants(client, product_id)
-        stock_units = client.get_skus_by_product_id product_id
+      def find_parent_sku(stock_units, product_id, client)
+        if unit = stock_units.find { |u| u[:id] == product_id }
+          {
+            sku: unit[:id],
+            price: unit[:price],
+            list_price: unit[:list_price],
+            cost_price: unit[:cost_price],
+            images: map_images(client, unit[:id])
+          }
+        else
+          {}
+        end
+      end
 
+      def map_variants(stock_units, product_id, client)
         stock_units.map do |stock_unit|
           stock_unit_id = stock_unit.delete(:id)
+          next if stock_unit_id == product_id
 
           {
             sku: stock_unit_id,
@@ -33,7 +51,7 @@ module VTEX
             cost_price: stock_unit.delete(:cost_price),
             images: map_images(client, stock_unit_id)
           }.merge stock_unit.except(:product_id)
-        end
+        end.compact
       end
 
       def map_images(client, stock_unit_id)
